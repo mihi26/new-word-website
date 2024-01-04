c74baf3c338062d921c1a96bd12f017be64e1c51
mor-tund <tu.nguyen@mor.com.vn>
2024-01-04 13:25:17 +0700

    [front] Fix npm run build for combine voices

--- src/components/common/TextToSpeech.tsx
+++ src/components/common/TextToSpeech.tsx
@@ -1,66 +1,63 @@
-import { useState, useEffect } from "react"
-import { RepeatIcon } from "../icons/RepeatIcon"
-import { VolumeUp } from "../icons/VolumeUp"
+import {useEffect, useState} from "react"
+import {RepeatIcon} from "../icons/RepeatIcon"
+import {VolumeUp} from "../icons/VolumeUp"
 // import Button from "./Button"
 
-const TextToSpeech = ({ text }) => {
-  const [isRepeat, setIsRepeat] = useState(false)
-  const [isPause] = useState(true)
-  const [utterance, setUtterance] = useState<any>(null)
-  const [voices, setVoices] = useState<any>([])
+const TextToSpeech = ({text}) => {
+    const [isRepeat, setIsRepeat] = useState(false)
+    const [isPause] = useState(true)
+    const [utterance] = useState<any>(null)
+    const [voices, setVoices] = useState<any>([])
 
-  useEffect(() => {
-    const synth = window.speechSynthesis
-    setVoices(synth.getVoices())
-    console.log(synth.getVoices())
-    return () => {
-      synth.cancel()
+    useEffect(() => {
+        const synth = window.speechSynthesis
+        setVoices(synth.getVoices())
+        return () => {
+            synth.cancel()
+        }
+    }, [text])
+
+    const handlePlay = () => {
+        if (isPause) {
+            const synth = window.speechSynthesis
+            text.map(data => {
+                const u = new SpeechSynthesisUtterance(data.text)
+                u.voice = voices.find(voice => voice.lang === data.key)!
+                synth.speak(u)
+            })
+        }
     }
-  }, [text])
 
-  const handlePlay = () => {
-    if (isPause) {
-      const synth = window.speechSynthesis
-      const voices = synth.getVoices()
-      console.log(voices)
-      text.map(data => {
-        let u = new SpeechSynthesisUtterance(data.text)
-        u.voice = data.key === 'vn' ? voices.find(voice => voice.lang === 'vi-VN') : voices.find(voice => voice.lang === 'en-US')
-        console.log(u.voice)
-        synth.speak(u)
-      })
+    const handleRepeat = () => {
+        if (isRepeat) {
+            setIsRepeat(false)
+        } else {
+            setIsRepeat(true)
+            utterance.onend = function () {
+                handlePlay()
+            }
+            handlePlay()
+        }
     }
-  }
 
-  const handleRepeat = () => {
-    const synth = window.speechSynthesis
-    if (isRepeat) {
-      setIsRepeat(false)
-    } else {
-      setIsRepeat(true)
-      utterance.onend = function () {
-        synth.speak(utterance)
-      }
-      synth.speak(utterance)
-    }
-  }
-
-  return (
-    <div className="flex gap-4 items-center">
-      <div className="flex gap-3 items-center cursor-pointer w-fit hover:bg-slate-100 rounded p-2" onClick={handlePlay}>
-        <div className="bg-[#066cfa] w-min rounded-3xl p-2">
-          <VolumeUp width={24} height={24} color="white" />
+    return (
+        <div className="flex gap-4 items-center">
+            <div className="flex gap-3 items-center cursor-pointer w-fit hover:bg-slate-100 rounded p-2"
+                 onClick={handlePlay}>
+                <div className="bg-[#066cfa] w-min rounded-3xl p-2">
+                    <VolumeUp width={24} height={24} color="white"/>
+                </div>
+                <div className="text-[#066cfa]">Play</div>
+            </div>
+            <div className="flex gap-3 items-center cursor-pointer w-fit hover:bg-slate-100 rounded p-2"
+                 onClick={handleRepeat}>
+                <div className="bg-[#066cfa] w-min rounded-3xl p-2">
+                    <RepeatIcon width={24} height={24} color="white"/>
+                </div>
+                <div className="text-[#066cfa]">Repeat</div>
+            </div>
         </div>
-        <div className="text-[#066cfa]">Play</div>
-      </div>
-      <div className="flex gap-3 items-center cursor-pointer w-fit hover:bg-slate-100 rounded p-2" onClick={handleRepeat}>
-        <div className="bg-[#066cfa] w-min rounded-3xl p-2">
-          <RepeatIcon width={24} height={24} color="white" />
-        </div>
-        <div className="text-[#066cfa]">Repeat</div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default TextToSpeech
--- src/store/reducer/word.ts
+++ src/store/reducer/word.ts
@@ -1,60 +1,66 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { RootState } from ".."
-import { setLoading } from "./loading"
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import {RootState} from ".."
+import {setLoading} from "./loading"
 import ApiClientWithToken from "../../api/api"
-import { IWord } from "../../types"
-import { convertQueryString } from "../../utils"
+import {IWord} from "../../types"
+import {convertQueryString} from "../../utils"
+
 interface IWordState {
-  words: IWord[]
-  textToSpeechWord: []
+    words: IWord[]
+    textToSpeechWord: ITextState[]
+}
+
+interface ITextState {
+    key: string,
+    text: string
 }
 
 const initialState: IWordState = {
-  words: [],
-  textToSpeechWord: [],
+    words: [],
+    textToSpeechWord: [],
 }
 
 export const getNewWords = createAsyncThunk(
-  "words/getWords",
-  async (payload: any | undefined, { rejectWithValue, dispatch }) => {
-    dispatch(setLoading(true))
-    try {
-      if (payload) {
-        const res = await ApiClientWithToken.get(
-          `word${convertQueryString(payload)}`
-        )
-        return res.data
-      }
-      const res = await ApiClientWithToken.get("word")
-      return res.data
-    } catch (error) {
-      return rejectWithValue(error)
-    } finally {
-      dispatch(setLoading(false))
+    "words/getWords",
+    async (payload: any | undefined, {rejectWithValue, dispatch}) => {
+        dispatch(setLoading(true))
+        try {
+            if (payload) {
+                const res = await ApiClientWithToken.get(
+                    `word${convertQueryString(payload)}`
+                )
+                return res.data
+            }
+            const res = await ApiClientWithToken.get("word")
+            return res.data
+        } catch (error) {
+            return rejectWithValue(error)
+        } finally {
+            dispatch(setLoading(false))
+        }
     }
-  }
 )
 
 export const wordSlice = createSlice({
-  name: "words",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(getNewWords.fulfilled, (state, { payload }) => {
-      state.words = payload.data
-      const arrayMeaning = []
-      payload.data.forEach((item, index) => {
-        arrayMeaning.push({key:'en', text: item.word})
-        item.definition.map((data, indexDef) => {
-          arrayMeaning.push({key: 'en', text: (indexDef === 0 ? 'Definition. ' : '') + data.meaning})
-          arrayMeaning.push({key: 'vn', text: data.meaningVN})
+    name: "words",
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(getNewWords.fulfilled, (state, {payload}) => {
+            state.words = payload.data
+            const arrayMeaning: ITextState[] = []
+            payload.data.forEach((item) => {
+                arrayMeaning.push({key: 'en-US', text: item.word})
+                item.definition.map((data, indexDef) => {
+                    arrayMeaning.push({key: 'en-US', text: (indexDef === 0 ? 'Definition. ' : '') + data.meaning})
+                    arrayMeaning.push({key: 'vi-VN', text: data.meaningVN})
+                })
+                arrayMeaning.push({key: 'en-US', text: 'Example. ' + item.example})
+                arrayMeaning.push({key: 'vi-VN', text: item.exampleVN})
+            })
+            state.textToSpeechWord = JSON.parse(JSON.stringify(arrayMeaning))
         })
-        arrayMeaning.push({key:'en', text: 'Example. ' + item.example})
-        arrayMeaning.push({key:'en', text: item.exampleVN})
-      })
-      state.textToSpeechWord = JSON.parse(JSON.stringify(arrayMeaning))
-    })
-  },
+    },
 })
 export const selectWords = (state: RootState) => state["words"]
 export default wordSlice.reducer
